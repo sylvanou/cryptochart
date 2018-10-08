21c6dc3bc34a829615e004963a4bfca57a613959
sylvanou <sylvanoumbac24@gmail.com>
2018-10-08 15:35:33 -0400

    added Aux to gitignore and removed from chart

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import { Line } from "react-chartjs-2";
 import axios from "axios";
-import Aux from "../../hoc/Aux";
 import { timeConverter } from "../timeConverter";
 import { ChartHeader } from "./ChartHeader";
 import { CurrentValue } from "../CurrentValue/CurrentValue";
